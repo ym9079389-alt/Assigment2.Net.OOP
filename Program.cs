26590aa989aedab6c80b26068f39a9e05a32fe7d
ym9079389-alt <ym9079389@gmail.com>
2026-09-13 21:45:56 +0300

    Q, 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
+            #region Q 1
+            #region a
+            //a) What is the difference between a class and a struct?
 
+            //Class is reference type and Support OOP features,
+            //Struct is value type and does not support OOP features.
+            #endregion
+
+            #region b
+            //b) Why are classes more suitable than structs for large applications?
+
+            //Because classes are reference types, they are more suitable for large applications as they can be easily managed in memory and support features like inheritance.
+            #endregion
+
+            #endregion
         }
     }
 }
